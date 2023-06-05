6a0516642ee8b64663ea95701deb86cbc0cc083f
Miguel Bartolomeu <bartolomeu.miguel@gmail.com>
2023-06-05 17:00:03 -0600

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_222417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "total_price"
+    t.bigint "user_id", null: false
+    t.bigint "venue_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
+    t.string "email"
+    t.string "password"
+    t.string "profile_img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "address"
+    t.string "description"
+    t.string "img_url"
+    t.integer "surface_area"
+    t.integer "daily_rate"
+    t.string "venue_type"
+    t.boolean "has_parking"
+    t.boolean "has_internet"
+    t.boolean "has_ac"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_venues_on_user_id"
+  end
+
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "venues"
+  add_foreign_key "venues", "users"
 end
