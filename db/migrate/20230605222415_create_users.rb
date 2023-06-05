30723bca7a0dff7169895f107855937493d3cc0b
salim atiyeh <salimatiyeh@gmail.com>
2023-06-05 16:41:34 -0600

    change date to bd and date data type

--- db/migrate/20230605222415_create_users.rb
+++ db/migrate/20230605222415_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.integer :age
+      t.date :birth_date
       t.string :email
       t.string :password
       t.string :profile_img_url
