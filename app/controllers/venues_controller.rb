b3c280e6f3bac979e32059d480599d39852f1e64
Miguel Bartolomeu <bartolomeu.miguel@gmail.com>
2023-06-06 17:08:56 -0600

    booking controller(new, update) and venues controller(new,create)

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,17 +1,23 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:new, :create, :edit, :update, :show]
+  before_action :set_booking, only: :destroy
 
   def index
     @venues = Venue.all
   end
 
   def show
-    
+    @booking = Booking.new
   end
 
   def new
+    @venue = Venue.new
   end
 
   def create
+    @venue = Venue.new(venue_params)
+    @venue.save
+    redirect_to venues_path, status: :see_other
   end
 
   def edit
@@ -21,5 +27,21 @@ class VenuesController < ApplicationController
   end
 
   def destroy
+    @venue.destroy
+    redirect_to venues_path, status: :see_other
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  def venue_params
+    params.require(:booking).permit(:address, :description, :img_url, :surface_area, :daily_rate, :venue_type, :has_parking, :has_internet, :has_ac, :user_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:venue_id])
   end
 end
