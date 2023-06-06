4af244834183e6c947884ec017d5bc6d3a09051d
Miguel Bartolomeu <bartolomeu.miguel@gmail.com>
2023-06-06 11:35:34 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,6 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'venues/index'
-  get 'venues/show'
-  get 'venues/new'
-  get 'venues/create'
-  get 'venues/edit'
-  get 'venues/update'
-  get 'venues/destroy'
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :venues do
+    resources :bookings
+  end
 end
