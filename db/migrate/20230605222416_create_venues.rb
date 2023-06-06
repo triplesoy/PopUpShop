0a28e0d6668aaf29fc2affc81da586db59651a44
Miguel Bartolomeu <bartolomeu.miguel@gmail.com>
2023-06-06 12:59:26 -0600

    db title venues

--- db/migrate/20230605222416_create_venues.rb
+++ db/migrate/20230605222416_create_venues.rb
@@ -1,6 +1,7 @@
 class CreateVenues < ActiveRecord::Migration[7.0]
   def change
     create_table :venues do |t|
+      t.string :title, null: false
       t.string :address
       t.string :description
       t.string :img_url
